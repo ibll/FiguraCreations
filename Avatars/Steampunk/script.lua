5423cfc1b56c1f8daefc9658e9fb8c761199f07b
TheGoodDude22 <61601241+ibll@users.noreply.github.com>
2023-05-10 11:20:19 -0500

    Init

--- Avatars/Steampunk/script.lua
+++ Avatars/Steampunk/script.lua
@@ -1,11 +1,18 @@
 vanilla_model.PLAYER:setVisible(false)
 
 Blink = require("blink")
+Basics = require("basics")
+
+function events.entity_init()
+    Basics.entity_init()
+end
 
 function events.tick()
     Blink.tick()
+    Basics.tick()
 end
 
 function events.render(delta, context)
     Blink.render(delta)
+    Basics.render()
 end
