1747c30e339e95a283bd13beea0e6a71afb1afdc
TheGoodDude22 <61601241+TheGoodDude22@users.noreply.github.com>
2023-05-10 02:00:33 -0500

    Added blinking

--- Avatars/Steampunk/script.lua
+++ Avatars/Steampunk/script.lua
@@ -1,1 +1,11 @@
 vanilla_model.PLAYER:setVisible(false)
+
+Blink = require("blink")
+
+function events.tick()
+    Blink.tick()
+end
+
+function events.render(delta, context)
+    Blink.render(delta)
+end
