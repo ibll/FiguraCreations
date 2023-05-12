463746141c15f8a5b0aadd7b6908aaff8dda51cb
TheGoodDude22 <61601241+ibll@users.noreply.github.com>
2023-05-11 19:11:07 -0500

    I think it works fully now

--- Avatars/Crab/script.lua
+++ Avatars/Crab/script.lua
@@ -6,6 +6,8 @@
 
 WALK_ANIMATION = animations.model.walkin
 FALL_ANIMATION = animations.model.fall
+HIDE_ANIMATION = animations.model.hide
+UNHIDE_ANIMATION = animations.model.unhide
 
 function events.entity_init()
 	LastHeldItem = "minecraft:air"
@@ -16,6 +18,23 @@ function events.entity_init()
 	vanilla_model.ELYTRA:setVisible(false)
 
 	nameplate.ENTITY:setPivot(0, 1, 0)
+
+    -- config loading
+    HideEnabled = false
+
+	-- action wheel
+	ActionWheelPg1 = action_wheel:newPage("Functions")
+	action_wheel:setPage(ActionWheelPg1)
+
+	HideToggleAction = ActionWheelPg1:newAction()
+		:item("minecraft:nautilus_shell")
+		:title("Hide")
+		:color(DISABLED_COLOR)
+		:onToggle(ToggleHide)
+		:toggleTitle("Stop Hiding")
+		:toggleColor(ENABLED_COLOR)
+    HideToggleAction:setToggled(HideEnabled)
+
 end
 
 function events.tick()
@@ -40,6 +59,10 @@ function events.tick()
 		FALL_ANIMATION:stop()
 	else
 		FALL_ANIMATION:play()
+	end
+
+	if player:getVelocity():length() >= 0.1 and HideEnabled then
+		ToggleHide(false)
 	end
 end
 
@@ -67,4 +90,63 @@ function events.render(delta, context)
 	if HeadRot.x < 0 then
 		models.model.HEAD.BASE:setRot(HeadRot.x, 0, 0)
 	end
+
+	if HideEnabled then
+		if HIDE_ANIMATION:getPlayState() == "STOPPED" then
+			HIDE_ANIMATION:setSpeed(1)
+			HIDE_ANIMATION:loop("HOLD")
+			HIDE_ANIMATION:play()
+			UNHIDE_ANIMATION:stop()
+		end
+	else
+		if math.abs(HIDE_ANIMATION:getTime()) >= HIDE_ANIMATION:getLength() then
+			HIDE_ANIMATION:stop()
+			UNHIDE_ANIMATION:play()
+		end
+	end
+
+	if math.abs(HIDE_ANIMATION:getTime()) >= HIDE_ANIMATION:getLength() and HideEnabled then
+		SetBodyVisibility(false)
+	else
+		SetBodyVisibility(true)
+	end
+
+	if HIDE_ANIMATION:getPlayState() == "PLAYING" then
+		vanilla_model.HELD_ITEMS:setVisible(false)
+	else
+		vanilla_model.HELD_ITEMS:setVisible(true)
+	end
+
+end
+
+function SetBodyVisibility(state)
+	models.model.MIMIC_TORSO.INNER:setVisible(state)
+	models.model.HEAD:setVisible(state)
+	models.model.RIGHT_ARM:setVisible(state)
+	models.model.LEFT_ARM:setVisible(state)
+	models.model.LEFT_CRAWLERS:setVisible(state)
+	models.model.RIGHT_CRAWLERS:setVisible(state)
+end
+
+function ToggleHide(state)
+	print("§bHidden§f: §e" .. PrintState(state))
+	HideEnabled = state
+	HideToggleAction:setToggled(HideEnabled)
+	Sync()
+end
+
+function PrintState(state)
+	if state then
+		return "§aEnabled"
+	else
+		return "§cDisabled"
+	end
+end
+
+function Sync()
+	pings.sync(HideEnabled)
+end
+
+function pings.sync(hideState)
+	HideEnabled = hideState
 end
