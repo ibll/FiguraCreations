dc056b23901dc3651e786c2a554da7a8b1bd7879
ibll <61601241+ibll@users.noreply.github.com>
2023-06-15 04:05:34 -0500

    Massive cleanup

--- Avatars/Prop Hunt/script.lua
+++ Avatars/Prop Hunt/script.lua
@@ -1,131 +1,110 @@
-vanilla_model.ALL:setVisible(false)
-
-ENABLED_COLOR = vectors.hexToRGB("#a6e3a1")
-DISABLED_COLOR = vectors.hexToRGB("#f38ba8")
+-- figura functions
 
 function events.entity_init()
-    Tick = 0
-    StillTimer = 0
-    Snap = false
-    Popped = false
-    OldPosBlock = vec(0, 0, 0)
+    -- setup
+    vanilla_model.ALL:setVisible(false)
+    renderer:setShadowRadius(0)
+    nameplate.ENTITY:setVisible(false)
 
-    -- config loading
-    SnappingDisabled = false
-    if config:load("SnappingDisabled") == true then SnappingDisabled = true end
-    SnapFloor = false
-    if config:load("SnapFloor") == true then SnapFloor = true end
+    -- vars
+    SyncTick = 0
+    SnapMode = "Rounded"
+    TicksInSameBlock = 0
+    SoundEffectPlayed = false
 
     -- action wheel
 	ActionWheelPg1 = action_wheel:newPage("Functions")
 	action_wheel:setPage(ActionWheelPg1)
 
-    SnapToggleAction = ActionWheelPg1:newAction()
-        :item("minecraft:magenta_glazed_terracotta")
-        :title("Disable Snapping")
-        :color(ENABLED_COLOR)
-        :onToggle(ToggleSnap)
-        :toggleTitle("Enable Snapping")
-        :toggleColor(DISABLED_COLOR)
-    SnapToggleAction:setToggled(SnappingDisabled)
-
-    AlignmentToggleAction = ActionWheelPg1:newAction()
-        :item("minecraft:black_carpet")
-        :title("Enable Floor Snapping")
-        :color(DISABLED_COLOR)
-        :onToggle(ToggleAlignment)
-        :toggleTitle("Enable Round Snapping")
-        :toggleColor(ENABLED_COLOR)
-    AlignmentToggleAction:setToggled(SnapFloor)
-
-    -- setup
-    renderer:setShadowRadius(0)
-    nameplate.ENTITY:setVisible(false)
+    SnapModeAction = ActionWheelPg1:newAction()
+        :item("minecraft:ender_pearl")
+        :title("Snap Mode: Rounded")
+        :onLeftClick(CycleSnapMode)
 end
 
 function events.tick()
-    local vel = player:getVelocity()
-    local pos = player:getPos()
-    if SnapFloor then
-        NewPosBlock = pos:floor()
-    else
-        NewPosBlock = vec(math.floor(pos.x), math.round(pos.y), math.floor(pos.z))
-    end
-
-    if OldPosBlock == NewPosBlock then
-        StillTimer = StillTimer + 1
-    else
-        StillTimer = 0
-    end
-
-    if StillTimer >= 20 and not SnappingDisabled then
-        if NewPosBlock ~= OldPosBlock then
-            StillTimer = 0
-            return
-        end
-
-        models.model:setParentType("WORLD")
-        if SnapFloor then
-            BlockPos = vec(math.floor(pos.x)*16 + 8, math.floor(pos.y)*16, math.floor(pos.z)*16 + 8)
-        else
-            BlockPos = vec(math.floor(pos.x)*16 + 8, math.round(pos.y)*16, math.floor(pos.z)*16 + 8)
-        end
-        models.model:setPos(BlockPos)
-
-        if Popped == false then
-            sounds:playSound("minecraft:ui.button.click", player:getPos(), 1, 2, false)
-            Popped = true
-        end
-    else
-        models.model:setParentType("None")
-        models.model:setPos(0, 0, 0)
-        Popped = false
-    end
-
-    OldPosBlock = NewPosBlock
-
-    function SyncTick()
-        Tick = Tick + 1
-        if Tick < 200 then return end
-        if not host:isHost() then return end
-        Sync()
-        Tick = 0
-    end
-    SyncTick()
+    ModelPos()
+    Sync()
 end
 
 -- helpers
 
+function ModelPos() 
+    local pos = player:getPos()
+    if SnapMode == "Floored" then
+        CurrentPosition = pos:floor()
+    else
+        CurrentPosition = vec(math.floor(pos.x), math.round(pos.y), math.floor(pos.z))
+    end
+
+    if OldPosition == CurrentPosition then
+        TicksInSameBlock = TicksInSameBlock + 1
+    else
+        TicksInSameBlock = 0
+    end
+
+    if TicksInSameBlock >= 20 and SnapMode ~= "Disabled" then
+        models.model:setParentType("WORLD")
+        if SnapMode == "Floored" then
+            BlockPos = vec(math.floor(pos.x)*16 + 8, math.floor(pos.y)*16, math.floor(pos.z)*16 + 8)
+        elseif SnapMode == "Rounded" then
+            BlockPos = vec(math.floor(pos.x)*16 + 8, math.round(pos.y)*16, math.floor(pos.z)*16 + 8)
+        end
+        models.model:setPos(BlockPos)
+
+        if SoundEffectPlayed == false then
+            sounds:playSound("minecraft:ui.button.click", player:getPos(), 1, 2, false)
+            SoundEffectPlayed = true
+        end
+    else
+        models.model:setParentType("None")
+        models.model:setPos(0, 0, 0)
+        SoundEffectPlayed = false
+    end
+
+    OldPosition = CurrentPosition
+end
+
+function CycleSnapMode()
+    if SnapMode == "Rounded" then
+        SnapMode = "Floored"
+        SnapModeAction:title("Snap Mode: Floored")
+        SnapModeAction:item("minecraft:black_carpet")
+    elseif SnapMode == "Floored" then
+        SnapMode = "Disabled"
+        SnapModeAction:title("Snap Mode: Disabled")
+        SnapModeAction:item("minecraft:barrier")
+    else 
+        SnapMode = "Rounded"
+        SnapModeAction:title("Snap Mode: Rounded")
+        SnapModeAction:item("minecraft:ender_pearl")
+    end
+    config:save("SnapMode", SnapMode)
+    pings.sync(SnapMode)
+    PrintState("Snapping",  SnapMode)
+end
+
 function PrintState(string, state)
-    function State(bool)
-        if bool then return "§aEnabled" else return "§cDisabled" end
+    function State(value)
+        if value == true or value == false then
+            if value then return "§aEnabled" else return "§cDisabled" end
+        else
+            return "§b" .. value
+        end
     end
     print(string .. ": " .. (State(state)))
 end
 
--- ping inbetweens
-
 function Sync()
-    pings.sync(SnappingDisabled, SnapFloor)
-end
-
-function ToggleSnap()
-    SnappingDisabled = not SnappingDisabled
-    config:save("SnappingDisabled", SnappingDisabled)
-    Sync()
-    PrintState("Snapping", not SnappingDisabled)
-end
-
-function ToggleAlignment()
-    SnapFloor = not SnapFloor
-    config:save("SnapFloor", SnapFloor)
-    Sync()
-    PrintState("Floor Snapping", SnapFloor)
+    SyncTick = SyncTick + 1
+    if SyncTick < 200 then return end
+    if not host:isHost() then return end
+    pings.sync(SnapMode)
+    SyncTick = 0
 end
 
 -- pings
 
-function pings.sync(SnappingDisabledState, SnapFloorState)
-    SnappingDisabled = SnappingDisabledState
-    SnapFloor = SnapFloorState
+function pings.sync(SnapState)
+    SnapMode = SnapState
 end
