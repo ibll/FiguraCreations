d443725f650eefeaf2d10714954ccbe7ed95bd69
ibll <61601241+ibll@users.noreply.github.com>
2023-06-24 04:05:00 -0500

    fixed nameplate, added vomit emotes :P

--- Avatars/Lovely Horse/rise.lua
+++ Avatars/Lovely Horse/rise.lua
@@ -81,28 +81,14 @@ function riseAPI.riseTick()
 	else
 		models.Horsey.LowerHalf:setPos(0, 0, 0)
 	end
-
-	if RISE_ANIMATION:getPlayState() == "STOPPED"  and FALL_ANIMATION:getTime() >= FALL_ANIMATION:getLength() then
-		nameplate.ENTITY:setPos(0, 0.2, 0)
-	else
-		nameplate.ENTITY:setPos(0, 1, 0)
-	end
-
     lazySync()
 end
 
 function riseAPI.riseRender()
-	local height
-	if RISE_ANIMATION:getPlayState() == "PLAYING" then
-		height = math.lerp(0, 1, RISE_ANIMATION:getTime()/RISE_ANIMATION:getLength())
-	else
-		local fallTime = FALL_ANIMATION:getTime()
-		if fallTime == 0 then height = 0 return end
-		height = 1 - math.lerp(0, 1, FALL_ANIMATION:getTime()/FALL_ANIMATION:getLength())
-	end
-
-	renderer:setOffsetCameraPivot(0, height, 0)
-	nameplate.ENTITY:setPos(0, height * 2/3 + 0.5, 0)
+    local offset = models.Horsey.Head:getAnimPos()/16
+	nameplate.ENTITY:setPivot(offset.x, offset.y + 2.5, offset.z)
+	renderer:setOffsetCameraPivot(offset)
+    renderer:setOffsetCameraRot(-models.Horsey.Head:getAnimRot())
 end
 
 return riseAPI
