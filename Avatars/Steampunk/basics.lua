5be818886084c970fd489abee18ec88f72e36e78
ibll <61601241+ibll@users.noreply.github.com>
2023-06-25 21:45:55 -0500

    Steampunk

--- Avatars/Steampunk/basics.lua
+++ Avatars/Steampunk/basics.lua
@@ -1,147 +1,138 @@
 -- action wheel constants
-ENABLED_COLOR = vectors.hexToRGB("#a6e3a1")
-DISABLED_COLOR = vectors.hexToRGB("#f38ba8")
+local ENABLED_COLOR = vectors.hexToRGB("#a6e3a1")
+local DISABLED_COLOR = vectors.hexToRGB("#f38ba8")
 
-Tick = 0
+local tick = 0
 
-BasicsAPI = {}
+local armorEnabled
+local elytraEnabled
 
-function BasicsAPI.entity_init()
-    -- config loading
-    BustEnabled = false
-    if config:load("BustEnabled") == true then BustEnabled = true end
-    ArmorEnabled = false
-    if config:load("ArmorEnabled") == true then ArmorEnabled = true end
-    ElytraEnabled = false
-    if config:load("ElytraEnabled") == true then ElytraEnabled = true end
-
-    -- action wheel
-    ActionWheelPg1 = action_wheel:newPage("Functions")
-    action_wheel:setPage(ActionWheelPg1)
-
-    ArmorToggleAction = ActionWheelPg1:newAction()
-        :item("minecraft:netherite_chestplate")
-        :title("Enable Armour")
-        :color(DISABLED_COLOR)
-        :onToggle(ToggleArmor)
-        :toggleTitle("Disable Armour")
-        :toggleColor(ENABLED_COLOR)
-    ArmorToggleAction:setToggled(ArmorEnabled)
-
-    ElytraToggleAction = ActionWheelPg1:newAction()
-        :item("minecraft:elytra")
-        :title("Enable Elytra")
-        :color(DISABLED_COLOR)
-        :onToggle(ToggleElytra)
-        :toggleTitle("Disable Elytra")
-        :toggleColor(ENABLED_COLOR)
-    ElytraToggleAction:setToggled(ElytraEnabled)
-
-    BustToggleAction = ActionWheelPg1:newAction()
-        :item("minecraft:melon")
-        :title("Enable Bust")
-        :color(DISABLED_COLOR)
-        :onToggle(ToggleBust)
-        :toggleTitle("Disable Bust")
-        :toggleColor(ENABLED_COLOR)
-    BustToggleAction:setToggled(BustEnabled)
-
-    Sync()
-end
-
-function BasicsAPI.tick()
-    Tick = Tick + 1
-    if Tick < 200 then return end
-    if not host:isHost() then return end
-
-    Sync()
-    Tick = 0
-end
-
-function BasicsAPI.render()
-    function BustClipping()
-        function ModelVisibility(bool)
-            models.Steampunk.Body.Bust:setVisible(bool)
-        end
-
-        if not BustEnabled then return ModelVisibility(false) end
-        if player:getItem(5).id == "minecraft:air" then return ModelVisibility(true) end
-        if player:getItem(5).id == "minecraft:elytra" then return ModelVisibility(true) end
-        if not vanilla_model.CHESTPLATE:getVisible() then return ModelVisibility(true) end
-        ModelVisibility(false)
-    end
-    BustClipping()
-
-    function BootClipping()
-        function ModelVisibility(bool)
-            models.Steampunk.LeftLeg.LeftShoe:setVisible(bool)
-            models.Steampunk.RightLeg.RightShoe:setVisible(bool)
-        end
-    
-        if player:getItem(3).id == "minecraft:air" then return ModelVisibility(true) end
-        if not vanilla_model.BOOTS:getVisible() then return ModelVisibility(true) end
-        ModelVisibility(false)
-    end
-    BootClipping()
-
-    -- Add other functions below!
-end
+local armorToggleAction
+local elytraToggleAction
 
 ---------------
 -- FUNCTIONS --
 ---------------
 
-function PrintState(string, state)
-    function State(bool)
-        if bool then return "§aEnabled" else return "§cDisabled" end
+local function printState(string, state)
+    local function fancyState(value)
+        if value == true or value == false then
+            if value then return "§aEnabled" else return "§cDisabled" end
+        else
+            return "§b" .. value
+        end
     end
-    print(string .. ": " .. (State(state)))
+    print(string .. ": " .. (fancyState(state)))
 end
 
-function ToggleArmor()
-    ArmorEnabled = not ArmorEnabled
-    config:save("ArmorEnabled", ArmorEnabled)
-    ArmorToggleAction:setToggled(ArmorEnabled)
-    Sync()
-    PrintState("Armour Visibility", ArmorEnabled)
+local function toggleArmor()
+    armorEnabled = not armorEnabled
+    config:save("ArmorEnabled", armorEnabled)
+    armorToggleAction:setToggled(armorEnabled)
+    quickSync()
+    printState("Armour Visibility", armorEnabled)
 end
 
-function ToggleElytra()
-    ElytraEnabled = not ElytraEnabled
-    config:save("ElytraEnabled", ElytraEnabled)
-    ElytraToggleAction:setToggled(ElytraEnabled)
-    Sync()
-    PrintState("Elytra Visibility", ElytraEnabled)
+local function toggleElytra()
+    elytraEnabled = not elytraEnabled
+    config:save("ElytraEnabled", elytraEnabled)
+    elytraToggleAction:setToggled(elytraEnabled)
+    quickSync()
+    printState("Elytra Visibility", elytraEnabled)
 end
 
-function ToggleBust()
-    BustEnabled = not BustEnabled
-    config:save("BustEnabled", BustEnabled)
-    BustToggleAction:setToggled(BustEnabled)
-    Sync()
-    PrintState("Bust Visibility", BustEnabled)
+local function quickSync()
+    pings.sync(armorEnabled, elytraEnabled)
 end
 
-function Sync()
-    pings.sync(ArmorEnabled, ElytraEnabled, BustEnabled)
-end
-
-function ModifyVisibility()
-    vanilla_model.ARMOR:setVisible(ArmorEnabled)
-    vanilla_model.ELYTRA:setVisible(ElytraEnabled)
+local function modifyVisibility()
+    vanilla_model.ARMOR:setVisible(armorEnabled)
+    vanilla_model.ELYTRA:setVisible(elytraEnabled)
 end
 
 -----------
 -- PINGS --
 -----------
 
-function pings.sync(armourState, elytraState, bustState)
-    ArmorEnabled = armourState
-    ElytraEnabled = elytraState
-    BustEnabled = bustState
-
-    ModifyVisibility()
-    --print("Pinging (A/E/B)", armourState, elytraState, bustState)
+function pings.sync(armourState, elytraState)
+    armorEnabled = armourState
+    elytraEnabled = elytraState
+    modifyVisibility()
 end
 
-return BasicsAPI
+---------
+-- API --
+---------
+
+local basicsAPI = {}
+
+function basicsAPI.entity_init()
+    -- config loading
+    armorEnabled = false
+    if config:load("ArmorEnabled") == true then armorEnabled = true end
+    elytraEnabled = false
+    if config:load("ElytraEnabled") == true then elytraEnabled = true end
+
+    -- action wheel
+    local functionsWheel = action_wheel:newPage("Functions")
+    action_wheel:setPage(functionsWheel)
+
+    armorToggleAction = functionsWheel:newAction()
+        :item("minecraft:netherite_chestplate")
+        :title("Enable Armour")
+        :color(DISABLED_COLOR)
+        :onToggle(toggleArmor)
+        :toggleTitle("Disable Armour")
+        :toggleColor(ENABLED_COLOR)
+    armorToggleAction:setToggled(armorEnabled)
+
+    elytraToggleAction = functionsWheel:newAction()
+        :item("minecraft:elytra")
+        :title("Enable Elytra")
+        :color(DISABLED_COLOR)
+        :onToggle(toggleElytra)
+        :toggleTitle("Disable Elytra")
+        :toggleColor(ENABLED_COLOR)
+    elytraToggleAction:setToggled(elytraEnabled)
+
+    quickSync()
+end
+
+function basicsAPI.tick()
+    tick = tick + 1
+    if tick < 200 then return end
+    if not host:isHost() then return end
+
+    quickSync()
+    tick = 0
+end
+
+function basicsAPI.render()
+    local function bustClipping()
+        local function modelVisibility(bool)
+            models.Steampunk.Body.Bust:setVisible(bool)
+        end
+
+        if player:getItem(5).id == "minecraft:air" then return modelVisibility(true) end
+        if player:getItem(5).id == "minecraft:elytra" then return modelVisibility(true) end
+        if not vanilla_model.CHESTPLATE:getVisible() then return modelVisibility(true) end
+        modelVisibility(false)
+    end
+    bustClipping()
+
+    local function bootClipping()
+        local function modelVisibility(bool)
+            models.Steampunk.LeftLeg.LeftShoe:setVisible(bool)
+            models.Steampunk.RightLeg.RightShoe:setVisible(bool)
+        end
+    
+        if player:getItem(3).id == "minecraft:air" then return modelVisibility(true) end
+        if not vanilla_model.BOOTS:getVisible() then return modelVisibility(true) end
+        modelVisibility(false)
+    end
+    bootClipping()
+
+    -- Add other functions below!
+end
+
+return basicsAPI
