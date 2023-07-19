238c4015ac5692876d704ee72a2811f0e2eb0486
ibll <61601241+ibll@users.noreply.github.com>
2023-07-19 13:49:13 -0500

    left bias works from front

--- Avatars/2D Ears/script.lua
+++ Avatars/2D Ears/script.lua
@@ -1,12 +1,14 @@
 local BILLBOARDED_MODELPART = models.model
+
+local LEFT_SIDE_OF_HEAD_BIAS = 25 -- in degrees
+local FLIP_TOLERANCE = 0 -- in degrees
 local LERP_TIME = 10 -- in ticks
-local FLIP_TOLERANCE = 10 -- in degrees
 
 local tick = 0
 local targetTick = 0
 local offset = 0
 local targetoffset = 0
-local earsRight = false
+local earsLeftOfScreen = true
 
 ----------------------
 -- Figura Functions --
@@ -19,26 +21,33 @@ function events.tick()
     local plaPos = player:getPos()
     local relDir = math.atan(camPos.z - plaPos.z, camPos.x - plaPos.x)*180/math.pi
     local hostDir = -math.shortAngle(player:getRot().y, -90)
-    local relativeAngle = math.shortAngle(hostDir, relDir)
+    local relAngle = math.shortAngle(hostDir, relDir)
+    local leftAngle = math.shortAngle(relAngle, -90)
+    local rightAngle = math.shortAngle(relAngle, 90)
 
     local function startFlip()
         targetTick = tick + LERP_TIME
 
         local offsetDiff = 180
-        if relativeAngle < 0 then offsetDiff = -offsetDiff end
+        if relAngle < LEFT_SIDE_OF_HEAD_BIAS then offsetDiff = -offsetDiff end
         -- comment next line to always flip away from camera
-        if math.abs(relativeAngle) > 90 then offsetDiff = -offsetDiff end
+        if math.abs(relAngle) > 90 then offsetDiff = -offsetDiff end
         targetoffset = targetoffset + offsetDiff
     end
 
-    local relAngPos = math.abs(relativeAngle)
+    local camIsLeft = leftAngle > -90 - LEFT_SIDE_OF_HEAD_BIAS + FLIP_TOLERANCE and leftAngle < 90 - LEFT_SIDE_OF_HEAD_BIAS - FLIP_TOLERANCE
+    local camIsRight = rightAngle < 90 - LEFT_SIDE_OF_HEAD_BIAS - FLIP_TOLERANCE and rightAngle > -90 - LEFT_SIDE_OF_HEAD_BIAS + FLIP_TOLERANCE
+    -- print(onLeftSide, ", ", onRightSide, ", ", leftAngle, ", ", world:getTime())
 
-    if relAngPos > FLIP_TOLERANCE and relAngPos < 180 - FLIP_TOLERANCE then
-        if relativeAngle < 0 ~= earsRight then
+    if camIsLeft or camIsRight then
+        local earsShouldBeLeftOfScreen = relAngle > LEFT_SIDE_OF_HEAD_BIAS
+        if  earsShouldBeLeftOfScreen ~= earsLeftOfScreen then
             startFlip()
+            -- print("Flip!")
         end
-        earsRight = relativeAngle < 0
+        earsLeftOfScreen = earsShouldBeLeftOfScreen
     end
+
 end
 
 function events.render(delta)
