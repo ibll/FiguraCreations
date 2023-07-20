3987d43d2ce3df17dfb50404a8121df885dc68f2
ibll <61601241+ibll@users.noreply.github.com>
2023-07-19 21:24:38 -0500

    all done!

--- Avatars/2D Ears/script.lua
+++ Avatars/2D Ears/script.lua
@@ -1,14 +1,25 @@
+---------------------
+-- Modify these!!! --
+---------------------
+-- i'm really sorry these are a mess, it's super
+-- hard to describe much of the behaviour in this.
+
+-- anyway, you can get pretty much any behaviour
+-- you want by changing these.
+
+-- if you don't like how it's billboarded, the offset
+-- value in the render function is the actual var
+-- everything else is calculating for you.
+
 local BILLBOARDED_MODELPART = models.model
 
-local BIAS_RELATIVE_TO_SCREEN = false
--- if you want the ears to prefer staying towards
--- one side of the *head*, set this false.
--- if you want the ears to prefer staying towards
--- one side of the *viewers screen*, set this true.
-
-local LEFT_BIAS = 0
--- in degrees, range where ears will prefer one
--- side of the head/screen. can be -90 through 90.
+local FRONT_ANGLE = 0
+-- the angle the ears should try to avoid.
+-- ears will keep as close back as possible.
+-- To stay back, set to 0
+-- To stay right, set to 90
+-- To stay front, set to 180
+-- To stay left, set to 270
 
 local FLIP_TOLERANCE = 10
 -- in degrees, the deadzone space where the ears
@@ -21,12 +32,26 @@ local LERP_TIME = 10
 -- travel from one side to the other. there are
 -- 20 ticks in a second.
 
-local FLIP_DIR = "AWAY_FROM_FACE"
--- local FLIP_DIR = "TOWARDS_FACE"
+local FLIP_DIR = "AWAY_FROM_FRONT"
+-- local FLIP_DIR = "TOWARDS_FRONT"
 -- local FLIP_DIR = "AWAY_FROM_CAMERA"
 -- local FLIP_DIR = "TOWARDS_CAMERA"
 -- local FLIP_DIR = "CLOCKWISE"
 -- local FLIP_DIR = "COUNTERCLOCKWISE"
+
+local BIAS_RELATIVE_TO_SCREEN = false
+-- if you want the ears to prefer staying towards
+-- one side of the *head*, set this false.
+-- if you want the ears to prefer staying towards
+-- one side of the *viewers screen*, set this true.
+
+local LEFT_BIAS = 0
+-- in degrees, range where ears will prefer one
+-- side of the head/screen. can be -90 through 90.
+
+----------
+-- Vars --
+----------
 
 local tick = 0
 local targetTick = 0
@@ -45,7 +70,7 @@ function events.tick()
     local plaPos = player:getPos()
     local relDir = math.atan(camPos.z - plaPos.z, camPos.x - plaPos.x)*180/math.pi
     local hostDir = -math.shortAngle(player:getRot().y, -90)
-    local relAngle = math.shortAngle(hostDir, relDir)
+    local relAngle = math.shortAngle(hostDir, relDir + FRONT_ANGLE)
     local leftAngle = math.shortAngle(relAngle, -90)
     local rightAngle = math.shortAngle(relAngle, 90)
     
@@ -69,10 +94,10 @@ function events.tick()
         targetTick = tick + LERP_TIME
 
         local offsetDiff = 180
-        if FLIP_DIR == "AWAY_FROM_FACE" then
+        if FLIP_DIR == "AWAY_FROM_FRONT" then
             if earsLeftOfScreen then offsetDiff = -offsetDiff end
             if math.abs(relAngle) > 90 then offsetDiff = -offsetDiff end
-        elseif FLIP_DIR == "TOWARDS_FACE" then
+        elseif FLIP_DIR == "TOWARDS_FRONT" then
             if not earsLeftOfScreen then offsetDiff = -offsetDiff end
             if math.abs(relAngle) > 90 then offsetDiff = -offsetDiff end
         elseif FLIP_DIR == "AWAY_FROM_CAMERA" then
