76c66b89bed7050390f8750a41816abdd581c1f5
ibll <61601241+ibll@users.noreply.github.com>
2023-07-19 20:46:49 -0500

    more options

--- Avatars/2D Ears/script.lua
+++ Avatars/2D Ears/script.lua
@@ -1,8 +1,32 @@
 local BILLBOARDED_MODELPART = models.model
 
-local LEFT_SIDE_OF_HEAD_BIAS = 0 -- in degrees, -90-90
-local FLIP_TOLERANCE = 10 -- in degrees
-local LERP_TIME = 10 -- in ticks
+local BIAS_RELATIVE_TO_SCREEN = false
+-- if you want the ears to prefer staying towards
+-- one side of the *head*, set this false.
+-- if you want the ears to prefer staying towards
+-- one side of the *viewers screen*, set this true.
+
+local LEFT_BIAS = 0
+-- in degrees, range where ears will prefer one
+-- side of the head/screen. can be -90 through 90.
+
+local FLIP_TOLERANCE = 10
+-- in degrees, the deadzone space where the ears
+-- won't flip. when set to 0, slightly moving the
+-- camera from left to right will cause repeated
+-- flipping
+
+local LERP_TIME = 10
+-- in ticks, the time it takes for the ears to
+-- travel from one side to the other. there are
+-- 20 ticks in a second.
+
+local FLIP_DIR = "AWAY_FROM_FACE"
+-- local FLIP_DIR = "TOWARDS_FACE"
+-- local FLIP_DIR = "AWAY_FROM_CAMERA"
+-- local FLIP_DIR = "TOWARDS_CAMERA"
+-- local FLIP_DIR = "CLOCKWISE"
+-- local FLIP_DIR = "COUNTERCLOCKWISE"
 
 local tick = 0
 local targetTick = 0
@@ -25,17 +49,40 @@ function events.tick()
     local leftAngle = math.shortAngle(relAngle, -90)
     local rightAngle = math.shortAngle(relAngle, 90)
     
-    local camIsLeft = leftAngle > -90 - LEFT_SIDE_OF_HEAD_BIAS + FLIP_TOLERANCE and leftAngle < 90 - LEFT_SIDE_OF_HEAD_BIAS - FLIP_TOLERANCE
-    local camIsRight = rightAngle < 90 - LEFT_SIDE_OF_HEAD_BIAS - FLIP_TOLERANCE and rightAngle > -90 - LEFT_SIDE_OF_HEAD_BIAS + FLIP_TOLERANCE
-    local earsShouldBeLeftOfScreen = (leftAngle < -90 - LEFT_SIDE_OF_HEAD_BIAS) or (leftAngle > 90 - LEFT_SIDE_OF_HEAD_BIAS)
+    local camIsLeft
+    local camIsRight
+    local earsShouldBeLeftOfScreen
+
+    if BIAS_RELATIVE_TO_SCREEN == false then
+        camIsLeft = leftAngle > -90 - LEFT_BIAS + FLIP_TOLERANCE and leftAngle < 90 - LEFT_BIAS - FLIP_TOLERANCE
+        camIsRight = rightAngle < 90 - LEFT_BIAS - FLIP_TOLERANCE and rightAngle > -90 - LEFT_BIAS + FLIP_TOLERANCE
+        earsShouldBeLeftOfScreen = (leftAngle < -90 - LEFT_BIAS) or (leftAngle > 90 - LEFT_BIAS)
+        
+    else
+        camIsLeft = leftAngle > -90 + LEFT_BIAS + FLIP_TOLERANCE and leftAngle < 90 - LEFT_BIAS - FLIP_TOLERANCE
+        camIsRight = rightAngle < 90 + LEFT_BIAS - FLIP_TOLERANCE and rightAngle > -90 - LEFT_BIAS + FLIP_TOLERANCE
+        earsShouldBeLeftOfScreen = (leftAngle < -90 + LEFT_BIAS) or (leftAngle > 90 - LEFT_BIAS)
+
+    end
 
     local function startFlip()
         targetTick = tick + LERP_TIME
 
         local offsetDiff = 180
-        if earsLeftOfScreen then offsetDiff = -offsetDiff end
-        -- comment next line to always flip away from camera
-        if math.abs(relAngle) > 90 then offsetDiff = -offsetDiff end
+        if FLIP_DIR == "AWAY_FROM_FACE" then
+            if earsLeftOfScreen then offsetDiff = -offsetDiff end
+            if math.abs(relAngle) > 90 then offsetDiff = -offsetDiff end
+        elseif FLIP_DIR == "TOWARDS_FACE" then
+            if not earsLeftOfScreen then offsetDiff = -offsetDiff end
+            if math.abs(relAngle) > 90 then offsetDiff = -offsetDiff end
+        elseif FLIP_DIR == "AWAY_FROM_CAMERA" then
+            if earsLeftOfScreen then offsetDiff = -offsetDiff end
+        elseif FLIP_DIR == "TOWARDS_CAMERA" then
+            if not earsLeftOfScreen then offsetDiff = -offsetDiff end
+        elseif FLIP_DIR == "CLOCKWISE" then
+            offsetDiff = -offsetDiff
+        end
+
         targetoffset = targetoffset + offsetDiff
     end
     
@@ -47,7 +94,7 @@ function events.tick()
         earsLeftOfScreen = earsShouldBeLeftOfScreen
     end
     
-    -- print(camIsLeft, ", ", camIsRight, ", ", relAngle, ", ", world:getTime())
+    print(camIsLeft, ", ", camIsRight, ", ", relAngle, ", ", world:getTime())
 end
 
 function events.render(delta)
